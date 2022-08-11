b08623e8a05f892d24b2009bd2c492e654664d35
HyoJin <hyodii9736@gmail.com>
2022-08-11 14:48:31 +0900

    20220811 shoppingmall

--- shoppingmall/src/page/ProductAll.js
+++ shoppingmall/src/page/ProductAll.js
@@ -1,11 +1,16 @@
 import React, {useEffect, useState} from 'react'
 import ProductCard from "../component/ProductCard";
 import {Col, Container, Row} from "react-bootstrap";
+import {useSearchParams} from "react-router-dom";
 
 const ProductAll = () => {
   const [prodectList, setProdectList] = useState([]);
+  const [query, setQuery] = useSearchParams();
   const getProducts = async () => {
-    let url = 'http://localhost:5001/products';
+    let searchQuery = query.get("q") || "";
+    console.log("쿼리값은?", searchQuery);
+    // let url = `https://my-json-server.typicode.com/hyodii/REACT_STUDY/shoppingmall/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/legobitna/hnm-react-router/products?q=${searchQuery}`;
     let response = await fetch(url)
     let data = await response.json();
     // console.log(data);
@@ -13,7 +18,7 @@ const ProductAll = () => {
   }
   useEffect(()=>{
     getProducts()
-  }, [])
+  }, [query])
   return 	<div>
     <Container>   {/*Container 컴포넌트는 가운데로 자동 정렬해줌*/}
       <Row>
